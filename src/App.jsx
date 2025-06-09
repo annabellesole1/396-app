29e44363d8dabccb7fe9018774228438d5d04a0a
annabellesole1 <annabellesole1@gmail.com>
2025-06-08 20:19:06 -0500

    Enable firebase and change styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Set state variables
+  const [profileText, setProfileText] = useState("")
+  const [result, setResult] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    // Prevents reload on form submission
+    e.preventDefault()
+    setLoading(true)
+    setResult(null)
+
+    const profileList = profileText.split(',').map(p => p.trim())
+    const candidateList = profileList[0].split('')
+
+    try {
+      // Calls GCF
+      const response = await fetch("https://singlepeaked-396-249233670711.us-central1.run.app", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          candidates: candidateList,
+          profile: profileList
+        })
+      })
+
+      const data = await response.json()
+      setResult(data.result || "Error")
+    } 
+    catch (error) {
+      setResult("Failed to contact server.")
+    } 
+    finally {
+      setLoading(false)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app">
+      <h1>Single-Peaked Preference Checker</h1>
+      <p>Enter a candidate profile.<br/>Write <i>abcdef</i> to indicate a vote where a is ranked first, b is ranked
+second, etc. <br/> Comma-separate each ranking, e.g. abcd,bdac,dcab.</p>
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: '20px' }}>
+          <input
+            type="text"
+            value={profileText}
+            onChange={(e) => setProfileText(e.target.value)}
+          />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? "Checking..." : "Submit"}
         </button>
+      </form>
+
+      {result && (
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Axis: {Array.isArray(result) ? result.join(" > ") : result}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+    </div>
   )
 }
 
